0a0d5db1d52abeeda4cf7d2be274ef3e01d4485e
Ian_S <iansalomao17@gmail.com>
2022-06-16 14:59:33 -0300

    Conexões
    Conexão com o banco de dados sql local.
    Criação da função de login.
    Criação da tela inicial.

---
+++ cadastro.php
@@ -0,0 +1,18 @@
+
+<h1>Cadastro</h1>
+<form id="form_cadastro">
+  <label for="">Nome</label>
+  <input type="text" id="nome" />
+  <label for="">CPF</label>
+  <input type="text" id="cpf" />
+  <label for="">Email</label>
+  <input type="email" id="email" />
+  <label for="">Senha</label>
+  <input type="password" id="senha" />
+  <label for="">cargo</label>
+  <select id="cargo">
+    <option>Administrador</option>
+    <option>Vendedor</option>
+  </select>
+  <input type="submit" form="form_cadastro" />
+</form>
---
+++ nav.php
@@ -0,0 +1,34 @@
+<nav class="navbar navbar-expand-lg" style="background-color: #1e1e1e">
+      <div class="container-fluid">
+        <h2 class="text-light mx-5 my-3" href="#">Loja</h2>
+        <button
+          class="navbar-toggler bg-light"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div
+          class="collapse navbar-collapse justify-content-end"
+          id="navbarNav"
+        >
+          <ul class="navbar-nav p-2">
+            <li class="nav-item">
+              <a class="nav-link text-light" aria-current="page" href="#"
+                >Produtos</a
+              >
+            </li>
+            <li class="nav-item">
+              <a class="nav-link text-light mx-2" href="#">Usuários</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link text-light mx-2" href="#">Vendas</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
